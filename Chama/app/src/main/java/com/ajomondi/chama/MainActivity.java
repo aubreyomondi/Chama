9156718a0ec1b404a15a028c5f5833b9611bc3d4
Aubrey J <aubreyomondi@gmail.com>
2020-10-14 00:25:58 +0300

    base app

---
+++ Chama/app/src/main/java/com/ajomondi/chama/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ajomondi.chama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
