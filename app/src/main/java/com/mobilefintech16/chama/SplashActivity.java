1fee343d45c288487c75d557a4f2e54f6e53bec6
Aubrey J <aubreyomondi@gmail.com>
2020-10-20 22:42:16 +0300

    Feature: Add splash screen

---
+++ app/src/main/java/com/mobilefintech16/chama/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.mobilefintech16.chama;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Duration of wait
+        int SPLASH_DISPLAY_LENGTH = 3000;
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                // Create an Intent that will start the MainActivity.
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
