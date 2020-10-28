cef50429741bd1b38d25a8632fea01f8de6cb581
Aubrey J <aubreyomondi@gmail.com>
2020-10-28 10:28:48 +0300

    fix firebase login bug

--- app/src/main/java/com/mobilefintech16/chama/LoginActivity.java
+++ app/src/main/java/com/mobilefintech16/chama/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +21,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    TextInputLayout username_text, password_text;
+    TextInputLayout id_text, password_text;
     Button callSignUp;
     ProgressBar progressBar;
 
@@ -44,20 +45,20 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        username_text = (TextInputLayout) findViewById(R.id.username_text);
+        id_text = (TextInputLayout) findViewById(R.id.id_text);
         password_text = (TextInputLayout) findViewById(R.id.password_text);
         progressBar = new ProgressBar(LoginActivity.this);
     }
 
-    private Boolean validateUserName(){
-        String val = username_text.getEditText().getText().toString();
+    private Boolean validateNationalId(){
+        String val = id_text.getEditText().getText().toString();
 
         if (val.isEmpty()) {
-            username_text.setError("Field cannot be empty");
+            id_text.setError("Field cannot be empty");
             return false;
         } else {
-            username_text.setError(null);
-            username_text.setErrorEnabled(false);
+            id_text.setError(null);
+            id_text.setErrorEnabled(false);
             return true;
         }
     }
@@ -77,7 +78,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void loginUser(View view){
 
-        if (!validateUserName() | !validatePassword())
+        if (!validateNationalId() | !validatePassword())
         {
             return;
         }else{
@@ -87,27 +88,29 @@ public class LoginActivity extends AppCompatActivity {
 
     private void isUser() {
         progressBar.setVisibility(View.VISIBLE);
-        final String userEnteredUsername = username_text.getEditText().getText().toString().trim();
+        final String userEnteredNationalId = id_text.getEditText().getText().toString().trim();
         final String userEnteredPassword = password_text.getEditText().getText().toString().trim();
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+        Query checkUser = reference.orderByChild("nationalId").equalTo(userEnteredNationalId);
         checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    username_text.setError(null);
-                    username_text.setErrorEnabled(false);
-                    String passwordFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
-                    if (passwordFromDB.equals(userEnteredPassword)) {
-                        username_text.setError(null);
-                        username_text.setErrorEnabled(false);
+                    id_text.setError(null);
+                    id_text.setErrorEnabled(false);
+                    String passwordFromDB = dataSnapshot.child(userEnteredNationalId).child("password").getValue().toString();
 
-                        String fullName = dataSnapshot.child(userEnteredUsername).child("fullName").getValue(String.class);
-                        String username = dataSnapshot.child(userEnteredUsername).child("username").getValue(String.class);
-                        String nationalId = dataSnapshot.child(userEnteredUsername).child("nationalId").getValue(String.class);
-                        String phoneNo = dataSnapshot.child(userEnteredUsername).child("phoneNo").getValue(String.class);
-                        String email = dataSnapshot.child(userEnteredUsername).child("email").getValue(String.class);
-                        String password = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
+                    if (passwordFromDB != null) {
+                    if (passwordFromDB.equals(userEnteredPassword)) {
+                        id_text.setError(null);
+                        id_text.setErrorEnabled(false);
+
+                        String fullName = dataSnapshot.child(userEnteredNationalId).child("fullName").getValue(String.class);
+                        String username = dataSnapshot.child(userEnteredNationalId).child("username").getValue(String.class);
+                        String nationalId = dataSnapshot.child(userEnteredNationalId).child("nationalId").getValue(String.class);
+                        String phoneNo = dataSnapshot.child(userEnteredNationalId).child("phoneNo").getValue(String.class);
+                        String email = dataSnapshot.child(userEnteredNationalId).child("email").getValue(String.class);
+                        String password = dataSnapshot.child(userEnteredNationalId).child("password").getValue(String.class);
 
                         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                         intent.putExtra("fullName", fullName);
@@ -122,10 +125,13 @@ public class LoginActivity extends AppCompatActivity {
                         password_text.setError("Wrong Password");
                         password_text.requestFocus();
                     }
+                    } else {
+                        Toast.makeText(LoginActivity.this,"Password from DB is empty",Toast.LENGTH_LONG).show();
+                    }
                 } else {
                     progressBar.setVisibility(View.GONE);
-                    username_text.setError("No such User exist");
-                    username_text.requestFocus();
+                    id_text.setError("No such User exist");
+                    id_text.requestFocus();
                 }
             }
 
